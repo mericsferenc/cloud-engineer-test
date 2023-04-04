0d0a2c892f395c6cbeecb8b98e17f0117a613f64
Merics Ferenc <mericsferenc@gmail.com>
2023-04-04 21:33:39 +0200

    Connect NestJS TypeORM with the Postgres database inside Docker

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Car } from './cars/car.entity';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CarsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [Car],
+      synchronize: true,
+    }),
+    CarsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
